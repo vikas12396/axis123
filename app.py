76f3a1b2c4be81e64fba3e07bc48afaebef4978b
vikas12396 <rpsvikash96@gmail.com>
2018-02-12 23:39:40 +0530

    next commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #from __future__ import print_function
 #from future.standard_library import install_aliases
 #install_aliases()
-
 from urllib.parse import urlparse, urlencode
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
@@ -20,18 +19,16 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+	return "Hello"
 	req = request.get_json(silent=True, force=True)
-
-    print("Request:")
-    print(json.dumps(req, indent=4))
-
-    res = processRequest(req)
-
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
+	print("Request:")
+	print(json.dumps(req, indent=4))
+	res = processRequest(req)
+	res = json.dumps(res, indent=4)
+	# print(res)
+	r = make_response(res)
+	r.headers['Content-Type'] = 'application/json'
+	return r
 
 
 def processRequest(req):
